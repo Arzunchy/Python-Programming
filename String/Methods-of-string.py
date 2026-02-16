fa53685cb158b90233f15f98314c5a5f11b381dc
Arjun Chaudhary <arzunchy338@gmail.com>
2026-02-17 01:28:49 +0530

    Learn python

---
+++ String/Methods-of-string.py
@@ -0,0 +1,24 @@
+##COMMON METHODS
+
+str="hello world"
+
+#Length of string
+print(len(str)) 
+
+#Remove Whitespace
+print(str.strip())
+
+#Uppercase
+print(str.upper)
+
+#Lowercase
+print(str.lower)
+
+#Replace substring
+print(str.repla2ce("world","python"))
+
+#Startswith find starting point like
+print(str.startswith("he")) #returns (true) coz it starts with he
+
+#Endswith
+print(str.endswith("ld")) # returns (true) it ends with
---
+++ String/Operation-of-string.py
@@ -0,0 +1,14 @@
+##CONCATENATION
+a="hello"
+b="world"
+print("hi","hello"+"world")
+
+##REPEATION
+
+a="hi"
+print("hi,"*4)
+
+##MEMBERSHIP
+
+a="python"
+print("py"in"python")
