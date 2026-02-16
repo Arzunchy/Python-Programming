c631ee808a6960c362e336ff26e9d4d7eeabdef9
Arjun Chaudhary <arzunchy338@gmail.com>
2026-02-17 01:10:34 +0530

    Learn python

---
+++ String/Access-of-string.py
@@ -0,0 +1,13 @@
+text="python"
+
+#indexing from 0
+print(text[0])
+
+#Negative indexing from -1 
+print(text[-1])
+
+#indexing 0 to 3
+print(text[0:3])
+
+#revsers string
+print(text[::-1])
---
+++ String/createstring.py
@@ -0,0 +1,5 @@
+str1 ="hello world"
+str2 ="hello world"
+
+multi_line=""" This is a multi_line string
+                                        """
