30551d32d5365908ce68787187df7580c8559ea5
Arjun Chaudhary <arzunchy338@gmail.com>
2026-02-19 02:54:22 +0530

    Learn python

--- LIST/REMOVINGElement.py
+++ LIST/REMOVINGElement.py
@@ -1,13 +1,21 @@
+#REMOVE we use element to remove it
+
 numbers=[10,20,30,40,50]
-numbers.remove[30]
+numbers.remove(30)
+print(numbers)
 
 #popped for last element
+numbers=[1,2,3,4,5]
 popped_value=numbers.pop()
+print(numbers)
 
 #popped for index value
+numbers=[1,2,3,4,5]
 popped_index_value=numbers.pop(2)
+print(numbers)
 
 #DELETE
+numbers=[1,2,3,4,5]
 del numbers[0]
 
 #CLEAR for remove all elements
--- LIST/SortingandReversing.py
+++ LIST/SortingandReversing.py
@@ -1,9 +1,14 @@
 numbers=[3,1,4,2]
 #Ascending order
 numbers.sort()
+print(numbers)
 
 #Descending order
+numbers=[2,4,3,1]
 numbers.sort(reverse=True)
+print(numbers)
 
-#reverse current order
+#Reverse current order
+numbers=[5,6,7,8,4]
 numbers.reverse()
+print(numbers)
