87e61ad1713c97030e18c0fe723b898a2f60e6f2
Arjun Chaudhary <arzunchy338@gmail.com>
2026-02-21 02:12:11 +0530

    Check Temperature Cold, Warm or Hot

---
+++ Programs/Phase 1/Level 1/Temperature.py
@@ -0,0 +1,8 @@
+Temperature=int(input("enter the temperature :"))
+
+if Temperature < 20:
+    print(F' Cold')
+elif Temperature <=20:   # another conditon 20<= temperature <=30 take between it
+    print(F'Warm')
+elif Temperature >=30:
+    print(F'Hot')        
