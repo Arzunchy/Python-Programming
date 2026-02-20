f7fd11cf0e1336cc4ae9500a89ff83cc42fd3a83
Arjun Chaudhary <arzunchy338@gmail.com>
2026-02-21 01:48:40 +0530

    Check Larger Number in Three

---
+++ Programs/Phase 1/Level 1/Larger in Three.py
@@ -0,0 +1,12 @@
+a=int(input("enter the first number :"))
+b=int(input("enter the second number :"))
+c=int(input("enter the third number :"))
+
+if a > b:
+    print(F'{a} is  Larger number :')
+elif b > a:
+    print(F'{b} is Larger number :')
+elif c > a:
+    print(F'{c} is Larger number :')
+else:
+    print(F'Thrice are equal :')        
