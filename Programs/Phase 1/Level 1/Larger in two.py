9ea80b3c5b913baa22cdc41d4d68a954ecb87a4b
Arjun Chaudhary <arzunchy338@gmail.com>
2026-02-21 01:42:01 +0530

    Check Larger Number in Two

---
+++ Programs/Phase 1/Level 1/Larger in two.py
@@ -0,0 +1,10 @@
+
+a=int(input("enter the first number : "))
+b=int(input("enter the second number : "))
+
+if a > b:
+    print(F'{a}Larger number is :')
+elif b > a:
+    print(F'{b}Larger number is :')
+else:
+    print(F'Both are equal')        
