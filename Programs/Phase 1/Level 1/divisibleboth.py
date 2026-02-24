6747a78b607585683d131cb4a1749b2d1c528a93
Arjun Chaudhary <arzunchy338@gmail.com>
2026-02-24 23:12:41 +0530

    Divisible By 5 & 3

--- Programs/Phase 1/Level 1/divisibleboth.py
+++ Programs/Phase 1/Level 1/divisibleboth.py
@@ -1,6 +1,6 @@
-nums=int(input("enter the  number : "))
+nums=int(input("Enter the number: "))
 
 if nums % 5 ==0 and nums % 3 ==0:
-    print("divible by 5")
+    print("Divisible by 5 and 3")
 else:
-    print("divible by 3")    
+    print("Not divisible")    
