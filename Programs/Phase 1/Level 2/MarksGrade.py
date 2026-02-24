30024545c44d5ef7bfb2a774250db35afb86491c
Arjun Chaudhary <arzunchy338@gmail.com>
2026-02-24 22:46:26 +0530

    Check Mraks Grade of Students

---
+++ Programs/Phase 1/Level 2/MarksGrade.py
@@ -0,0 +1,16 @@
+marks=float(input("Enter the marks : "))
+
+if marks >=90 and marks <=100:
+    print(F'Grade A+')
+elif marks >=80 and marks <90:
+    print(F'Grade A')
+elif marks >=70 and marks <80:
+    print(F'Grade B')
+elif marks >=60 and marks <70:
+    print(F'Grade C')
+elif marks >=40 and marks <60:
+    print(F'Grade D')
+else:
+    print(F'Fail')   
+
+
