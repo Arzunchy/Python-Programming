95708f4a907983c594a6e4f97ed5150319e5d508
Arjun Chaudhary <arzunchy338@gmail.com>
2026-02-24 23:15:26 +0530

    Check Odd And Even

--- Programs/Phase 1/Level 1/Od eve .py
+++
@@ -1,7 +0,0 @@
-
-nums=int(input("enter the nubmer : "))
-
-if nums % 2 ==0:
-    print("even")
-else:
-    print("odd")    
---
+++ Programs/Phase 1/Level 1/Odd-even .py
@@ -0,0 +1,6 @@
+nums=int(input("Enter the nubmer: "))
+
+if nums % 2 == 0:
+    print("Even")
+else:
+    print("Odd")    
