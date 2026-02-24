67ed5931c723fbfdec08e584779d49f467f58c51
Arjun Chaudhary <arzunchy338@gmail.com>
2026-02-24 23:17:17 +0530

    Check Positive & Negative

--- Programs/Phase 1/Level 1/Num check.py
+++
@@ -1,9 +0,0 @@
-
-nums=int(input("enter the number : "))
-
-if nums > 0:
-    print("positive")
-elif nums < 0:
-    print("negative")
-else:
-    print("zero")        
---
+++ Programs/Phase 1/Level 1/Num-check.py
@@ -0,0 +1,8 @@
+nums=int(input("Enter the number: "))
+
+if nums > 0:
+    print("Positive")
+elif nums < 0:
+    print("Negative")
+else:
+    print("Zero")        
