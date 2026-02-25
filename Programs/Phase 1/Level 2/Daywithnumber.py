c8ae1da9d5703a9e1ae7e9be471005836f5bb6e8
Arjun Chaudhary <arzunchy338@gmail.com>
2026-02-25 22:19:05 +0530

    Check Weeks day with number

---
+++ Programs/Phase 1/Level 2/Daywithnumber.py
@@ -0,0 +1,18 @@
+ch=int(input("Enter the number: "))
+
+if ch == 1:
+    print(F'Monday')
+elif ch == 2:
+    print(F'Tuesday')
+elif ch == 3:
+    print(F'Wednesday')
+elif ch == 4:
+    print(F'Thursday')
+elif ch == 5:
+    print(F'Friday')
+elif ch == 6:
+    print(F'Saturday')
+elif ch == 7:
+    print(F'Sunday')
+else:
+    print(F'No day')                            
