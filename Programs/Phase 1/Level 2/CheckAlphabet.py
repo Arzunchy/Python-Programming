a82352f9e78e968002d243a62d04b457bf0b07db
Arjun Chaudhary <arzunchy338@gmail.com>
2026-02-25 22:07:26 +0530

    Check Character Lies Between ' A 'to 'M' And 'N' to 'Z'

--- Programs/Phase 1/Level 2/CheckAlphabet.py
+++ Programs/Phase 1/Level 2/CheckAlphabet.py
@@ -5,4 +5,15 @@ if ch in ("abcdefghijklm"):
 elif ch in ("nopqrstuvwxyz"):
     print(F'It Lies Between n to z')
 else:
-    print(F'No Alphabets Lies')        
+    print(F'No Alphabets Lies') 
+
+
+#Another way with ((Range Checking method)) 
+ch=input("Enter the Charcater: ")
+
+if 'A' <= ch <= 'M':
+    print(F'Lies Between A to M')
+elif 'N' <= ch <= 'Z':
+    print(F'Lies Between N to Z')
+else:
+    print(F'No Character Lies ')      
