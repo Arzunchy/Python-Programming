ab3fca68a8b4b4089373cde6750c773b727308f8
Arjun Chaudhary <arzunchy338@gmail.com>
2026-02-25 21:22:16 +0530

    Greeting According to Hours of Day As "Good Morning"

---
+++ Programs/Phase 1/Level 2/Greetinghours.py
@@ -0,0 +1,15 @@
+Hours=float(input("Enter the Hours: "))
+
+if Hours >=6 and Hours < 12 :
+    print(F'Good Morning')
+elif Hours >= 12 and Hours < 17:
+    print(F'Good Afternoon')
+elif Hours >=17 and Hours < 23:
+    print(F'Good Evening')
+else:
+    print(F'Good Night')            
+
+
+
+
+
