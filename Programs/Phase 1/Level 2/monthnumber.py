cff487c981c4ce4cf6fc6c0e4077c13799f252f1
Arjun Chaudhary <arzunchy338@gmail.com>
2026-02-26 01:17:18 +0530

    Check month with number (1 to 12)

---
+++ Programs/Phase 1/Level 2/monthnumber.py
@@ -0,0 +1,28 @@
+month=int(input("Enter the month number(1 to 12): "))
+
+if month == 1:
+    print(F'January 31 days')
+elif month == 2:
+    print(F'February 28 days')
+elif month == 3:
+    print(F'March 31 days')
+elif month == 4:
+    print(F'April 30 days')
+elif month == 5:
+    print(F'May 31 days')
+elif month == 6:
+    print(F'June 30 days')
+elif month == 7:
+    print(F'July 31 days')
+elif month == 8:
+    print(F'August 31 days')   
+elif month == 9:
+    print(F'September 30 days')
+elif month == 10:
+    print(F'October 31 days')
+elif month == 11:
+    print(F'November 30 days')
+elif month == 12:
+    print(F'December 31 days')  
+else:
+    print(F'Invalid month')                       
