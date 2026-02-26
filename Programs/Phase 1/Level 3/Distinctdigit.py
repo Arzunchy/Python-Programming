38b2334c19192c6d0590091b5ce49522e922d17d
Arjun Chaudhary <arzunchy338@gmail.com>
2026-02-26 22:14:21 +0530

    Check digits are distinct or not

---
+++ Programs/Phase 1/Level 3/Distinctdigit.py
@@ -0,0 +1,9 @@
+#Distinct means digits should not equal if equal this is not distinct
+a=int(input("Enter the first digit(hundred): "))
+b=int(input("Enter the second digit(tens): "))
+c=int(input("Enter the third digit(ones): "))
+
+if a != b and b !=c and c != a:
+    print(F'Digits are distinct ')
+else:
+    print(F'Digits are not distinct')    
