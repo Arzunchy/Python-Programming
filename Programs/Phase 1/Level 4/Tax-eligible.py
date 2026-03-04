cc24d386db82dee412354a5c081383fbcf137aa3
Arjun Chaudhary <arzunchy338@gmail.com>
2026-03-04 21:15:43 +0530

    Check tax eligibility

---
+++ Programs/Phase 1/Level 4/Tax-eligible.py
@@ -0,0 +1,7 @@
+Age=int(input("Enter the age: "))
+Income=float(input("Enter the income: "))
+
+if 18 <= Age > 18 and 500000 <= Income > 500000:
+    print(F'Eligible for tax payment')
+else:
+    print(F'Not eligible for tax payemnt')   
