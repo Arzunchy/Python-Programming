d1f6f04ed2a5cff8c9b006a95e38a8ca0429d2d9
Arjun Chaudhary <arzunchy338@gmail.com>
2026-03-19 00:22:47 +0530

    Learning-dictionary

---
+++ Dictionary/Creating-dictionary/Direct-method.py
@@ -0,0 +1,15 @@
+#using curly braces
+dict={"name":"ram","age":20,"city":"bhopal"}
+print(dict)
+
+"""
+output={'name': 'ram', 'age': 20, 'city': 'bhopal'}
+"""
+
+#
+dict={"a":1,"b":2,"c":3}
+print(dict)
+
+"""
+output={'a': 1, 'b': 2, 'c': 3}
+"""
---
+++ Dictionary/Creating-dictionary/Using-dict-method.py
@@ -0,0 +1,3 @@
+#using dictionary constructor()
+my_dict=dict(Name="Ram",age=20,city="bhopal")
+print(my_dict)
