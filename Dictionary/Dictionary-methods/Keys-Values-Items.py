dca020aba291e1812e98e20c7dc056281a42cbc0
Arjun Chaudhary <arzunchy338@gmail.com>
2026-03-19 01:17:38 +0530

    Learning python

---
+++ Dictionary/Dictionary-methods/Keys-Values-Items.py
@@ -0,0 +1,20 @@
+#keys(),Values(),Items()
+
+d={"a":1,"b":2}   #return Keys only
+print(d.keys())
+"""output
+dict_keys(['a', 'b'])   
+"""
+
+d={"a":1,"b":2}  #return values only
+print(d.values())
+"""output
+([1,2])
+"""
+
+d={"a":1,"b":2} #return all items 
+print(d.items())
+"""output
+dict_items([('a', 1), ('b', 2)])
+"""
+
