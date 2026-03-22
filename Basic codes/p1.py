e1e40aa0a0272d77e7a4823a255f3edc2855a6b2
Arjun Chaudhary <arzunchy338@gmail.com>
2026-03-22 22:57:40 +0530

    Learning python

--- Basic codes/p1.py
+++ Basic codes/p1.py
@@ -12,10 +12,17 @@ print(fun(),fun(),fun())"""
 x.append([4,5])
 print(len(x))"""
 
-#return sort[1,2,3,4] without change [2,3]
+"""#return sort[1,2,3,4] without change [2,3]
 num=[2,2,1,1,3]
 num.sort()
-print(num)
+print(num)"""
+
+a,b="ab"
+c,d="cd"
+print(a,b,c)
+""""output
+a b c
+"""
 
 
 
