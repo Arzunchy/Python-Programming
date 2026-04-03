adce19406a75506fcfd4f3361116a9ec84f32b19
Arjun Chaudhary <arzunchy338@gmail.com>
2026-04-03 21:20:14 +0530

    Learning-python

--- Function/Creating-Function/def.py
+++ Function/Creating-Function/def.py
@@ -4,10 +4,21 @@
     return x + y
 print(add())
 """
-def palindrome():
+"""def palindrome():
     s="madam"
     s[::-1]
     return s==s
 print(palindrome())
+"""
+def fact(n):
+    if n == 0:
+        return 1
+    else:
+        return n*fact(n-1)
+n=5    
+print(fact(n))
+
+
+
 
 
